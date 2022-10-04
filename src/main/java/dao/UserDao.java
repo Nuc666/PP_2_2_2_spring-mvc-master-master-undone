6a58837841bbcb3907343e1d11525ad21da103c6
Sidorov Dmitry <Sidorovdmitri6@mail.ru>
2022-10-04 17:58:40 +0300

    Initial commit

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+import model.User;
+
+import java.util.List;
+
+public interface UserDao {
+   void add(User user);
+   List<User> listUsers();
+
+   void editUser(User modifiedUser, Long id);
+
+   void deleteUser(User user);
+
+   User getUser(Long id);
+
+
+}
---
+++ src/main/java/service/UserServiceImp.java
@@ -0,0 +1,44 @@
+package service;
+
+import dao.UserDao;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class UserServiceImp implements service.UserService {
+
+   @Autowired
+   private UserDao userDao;
+
+   @Transactional
+   @Override
+   public void add(User user) {
+      userDao.add(user);
+   }
+
+   @Transactional
+   @Override
+   public List<User> listUsers() {
+      return userDao.listUsers();
+   }
+
+   @Override
+   public void editUser(User modifiedUser, Long id) {
+      userDao.editUser(modifiedUser, id);
+   }
+
+   @Override
+   public void deleteUser(User user) {
+      userDao.deleteUser(user);
+   }
+
+   @Override
+   public User getUser(Long id) {
+     return userDao.getUser(id);
+   }
+
+
+}
